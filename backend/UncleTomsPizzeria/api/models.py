64a7f11d0da29b8400de0daeb6e1d0893611f29a
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2024-01-11 20:16:19 +0100

    Pizza models

--- backend/UncleTomsPizzeria/api/models.py
+++ backend/UncleTomsPizzeria/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Topping(models.Model):
+    name = models.CharField(max_length=32, verbose_name='Nazwa')
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=32, verbose_name='Nazwa')
+    toppings = models.ManyToManyField(Topping)
+    price_small = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Cena mała')
+    price_medium = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Cena średnia')
+    price_large = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Cena duża')
