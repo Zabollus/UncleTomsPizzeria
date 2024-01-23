3baf3614e888990c2dd5dac982a8900597403d59
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2024-01-23 15:17:55 +0100

    allowing headers for frontend

--- backend/UncleTomsPizzeria/UncleTomsPizzeria/settings.py
+++ backend/UncleTomsPizzeria/UncleTomsPizzeria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-omb3e%p74&2$8e9d^=%&n1q&%2(pxtk0_t7y@xyr03bak%mna9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
