a66c61f9b68d880097832e176c9e2d4a19b42c0b
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2024-02-08 17:21:57 +0100

    Bootstrap css working correctly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from "./components/Menu"
 import FormTopping from "./components/FormTopping"
 
 function App() {
   return (
-    <FormTopping/>
+    <>
+        <Menu/>
+        <FormTopping/>
+    </>
   );
 }
 
