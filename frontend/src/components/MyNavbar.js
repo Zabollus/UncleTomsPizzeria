11d62f90188dcb925011421df85b7339adc60372
Tomasz Jakubiak <tomek.jakubiak94@gmail.com>
2024-02-22 21:56:35 +0100

    MyNavbar component

---
+++ frontend/src/components/MyNavbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { Container } from 'react-bootstrap'
+
+function MyNavbar() {
+    return (
+        <Navbar bg='dark'>
+            <Container>
+                <Navbar.Brand href='/' style={{color:"white"}}>Uncle Toms Pizzeria</Navbar.Brand>
+                <Nav className='me-auto'>
+                    <Nav.Link href='add/pizza' style={{color:"white"}}>Dodaj pizzę<Nav.Link>
+                    <Nav.Link href='add/topping' style={{color:"white"}}>Dodaj składnik<Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default MyNavbar;
